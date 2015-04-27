4ed25b685e8d2c8f0b9d115f68cd5d2a240a6f80
ZakharovAA <zakharovaa@quarta.su>
2015-04-27 18:25:29 +0300

    add fixing styles

--- src/js/components/body.js
+++ src/js/components/body.js
@@ -2,8 +2,25 @@ var React = require('react'),
     Row = require('./row');
 
 var Body = React.createClass({
+    getInitialState: function () {
+        return {
+            width: 850
+        };
+    },
+    componentWillMount: function () {
+        window.addEventListener('resize', this.setWidth);
+    },
+    setWidth: function () {
+        this.setState({width: window.innerWidth});
+    },
     render: function () {
-        var rows = this.props.rows.map(function(row){
+        var renderRows = null;
+        if (this.props.rows.length > 100) {
+            renderRows = this.props.rows.slice(0, 100);
+        } else {
+            renderRows = this.props.rows;
+        }
+        var rows = renderRows.map(function (row) {
             return (<Row metadata={this.props.header} cells={row} />)
         }.bind(this));
         return (<div className="qtable__body">{rows}</div>);
